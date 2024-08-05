b64a53a3a45e453e95907f26c4716b7132dfa566
GitHub Action <action@github.com>
2024-08-05 04:03:58 +0000

    Initialize template repository proper
    Set package name to com.github.ptlthg.authtester
    Set project name to AuthTester
    Set modid to authtester

--- src/main/java/com/github/ptlthg/authtester/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/ptlthg/authtester/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.ptlthg.authtester.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
